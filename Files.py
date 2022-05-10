9d51cc9619082608fcf08de2c13037e6456b6764
Devesh Pal <newdev0@outlook.com>
2022-05-10 13:31:19 +0530

    Update

--- Files.py
+++ Files.py
@@ -70,84 +70,80 @@ async def fetch_files(word):
     print(f"---> Fetching {word}!")
     IgnoreWords.append(word)
     Content = {}
-    new_task = []
     folder = word[0].upper()
-    try:
-        async with ClientSession() as ses:
-            for filetype in Formats:
-                url = f"https://google.com/search?q={word}+filetype:{filetype}"
+    async with ClientSession() as ses:
+        for filetype in Formats:
+            url = f"https://google.com/search?q={word}+filetype:{filetype}"
 
-                async def get_page(url_):
-                    res = await ses.get(
-                      url_,
-                        headers={"User-Agent": generate_user_agent()},
-                 )
-                    ct = await res.read()
+            async def get_page(url_):
+                res = await ses.get(
+                    url_,
+                    headers={"User-Agent": generate_user_agent()},
+                )
+                ct = await res.read()
                 # open("test.html", "wb").write(ct)
-                    soup = BeautifulSoup(ct, "html.parser", from_encoding="utf-8")
-                    find = soup.find_all("div", re.compile("egMi0"))
-                    return find
+                soup = BeautifulSoup(ct, "html.parser", from_encoding="utf-8")
+                find = soup.find_all("div", re.compile("egMi0"))
+                return find
 
-                get_ = await get_page(url)
-                start = 0
-                while get_:
-                    for res in get_:
-                        try:
-                            name = res.find("div", re.compile("vvjwJb")).text
-                            fileurl = res.find("a", href=re.compile("/url?"))
-                            fileurl = (
+            get_ = await get_page(url)
+            start = 0
+            while get_:
+                for res in get_:
+                    try:
+                        name = res.find("div", re.compile("vvjwJb")).text
+                        fileurl = res.find("a", href=re.compile("/url?"))
+                        fileurl = (
                             fileurl["href"]
                             .split("url=")[1]
                             .split("&amp")[0]
                             .split("&ved=")[0]
                             .strip()
-                            )
-                            if fileurl not in IgnoreLinks:
-                                if not os.path.exists(folder):
-                                    os.mkdir(folder)
-                                if Content.get(filetype):
-                                    Content[filetype].append([name, fileurl])
-                                else:
-                                    Content.update({filetype: [[name, fileurl]]})
-                                IgnoreLinks.append(fileurl)
-                            print(f"--> GOT ---> FROM WORD --> {word}---> {fileurl}")
-                            await asyncio.sleep(2)
-                            for word_ in name.split():
-                                cont = True
-                                for c in word_:
-                                    if c not in string.ascii_letters:
-                                        cont = False
-                                        break
-                                if cont and len(word_) > 3 and word_ not in IgnoreWords:
+                        )
+                        if fileurl not in IgnoreLinks:
+                            if not os.path.exists(folder):
+                                os.mkdir(folder)
+                            if Content.get(filetype):
+                                Content[filetype].append([name, fileurl])
+                            else:
+                                Content.update({filetype: [[name, fileurl]]})
+                            IgnoreLinks.append(fileurl)
+                        print(f"--> GOT ---> FROM WORD --> {word}---> {fileurl}")
+                        await asyncio.sleep(2)
+                        for word_ in name.split():
+                            cont = True
+                            for c in word_:
+                                if c not in string.ascii_letters:
+                                    cont = False
+                                    break
+                            if cont and len(word_) > 3 and word_ not in IgnoreWords:
+                                if word not in Words:
                                     print(f"---> Got new word --> {word_}")
-                                    new_task.append(fetch_files(word_))
-                        except Exception as eR:
-                            # raise eR
-                            print(eR)
-                    if len(get_) == 10:
-                        start += 10
-                    await asyncio.sleep(random.randint(2, 5))
-                    get_ = await get_page(url + f"&start={start}")
-            _Processed.append(word)
-            if word in Words:
-                Words.remove(word)
-    except KeyboardInterrupt:
-        pass
+                                    Words.append(word)
+                    except Exception as eR:
+                        # raise eR
+                        print(eR)
+                if len(get_) == 10:
+                    start += 10
+                await asyncio.sleep(random.randint(2, 5))
+                get_ = await get_page(url + f"&start={start}")
+    _Processed.append(word)
+    if word in Words:
+        Words.remove(word)
     if Content:
         word = folder + word[1:]
         if not os.path.exists(folder + f"/{word}"):
             os.mkdir(folder + f"/{word}")
         save_files(Content, path=f"{folder}/{word}/")
-    await asyncio.gather(*new_task)
 
 
 async def main():
     print("> Starting UP!")
     await get_random_words_from_api()
     # task = []
-    for word in Words:
+    while Words:
         await asyncio.sleep(3)
-        await fetch_files(word)
+        await fetch_files(Words[0])
     # await asyncio.gather(*task)
 
 
